a43c2dc3a81a824556ec98b403d1d03a67c9c4bd
TeJ VarshIth <116535984+Varshi45@users.noreply.github.com>
2024-11-05 11:57:20 +0530

    update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,24 +1,20 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-// Create a new Sequelize instance with your PostgreSQL connection details
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "postgres", // Using PostgreSQL
-    port: process.env.DB_PORT || 5432, // Default PostgreSQL port
-    logging: false, // Disable logging (optional)
-  }
-);
+// Construct the connection string using environment variables
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+
+// Create a new Sequelize instance with the connection string
+const sequelize = new Sequelize(connectionString, {
+  dialect: "postgres", // Using PostgreSQL
+  logging: false, // Disable logging (optional)
+});
 
 // Test the connection
 const connectDB = async () => {
   try {
     await sequelize.authenticate();
-    console.log("Connection has been established and synced successfully.");
+    console.log("Connection has been established successfully.");
   } catch (error) {
     console.error("Unable to connect to the database:", error);
   }
