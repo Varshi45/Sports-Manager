839421748734398973e890591869f395bc31b77e
TeJ VarshIth <116535984+Varshi45@users.noreply.github.com>
2024-11-05 12:16:33 +0530

    update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 // Construct the connection string using environment variables
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+const connectionString = process.env.DB_URL;
 
 // Create a new Sequelize instance with the connection string
 const sequelize = new Sequelize(connectionString, {
